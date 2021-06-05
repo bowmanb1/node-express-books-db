d32c74e0017d776d54d0011885f5e9e45a946716
bowmanb1 <59579852+bowmanb1@users.noreply.github.com>
2021-06-05 12:46:03 -0700

    Create database.js

---
+++ database.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+// Where does this come from?
+const mongoDB = process.env.MONGODB_URI || 'mongodb+srv://user-13:m97zcguwhakwp4@cluster0.bzm1t.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+
+// What is this syntax about?
+mongoose
+    .connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => {
+        console.log('DB Connected!');
+    })
+    .catch(error => {
+        console.log('Connection Error: ${err.message}');
+    });
+
+const db = mongoose.connection;
+
+// Bind the console to errors, to show them on console
+db.on('error', console.error.bind(console, 'MongoDB Connection Error'));
+
+module.exports = db;
