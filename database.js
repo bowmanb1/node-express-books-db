cb19434607381bd803b7e5de9030006bd7319fcc
bowmanb1 <59579852+bowmanb1@users.noreply.github.com>
2021-06-05 15:06:51 -0700

    Update database.js

--- database.js
+++ database.js
@@ -1,3 +1,14 @@
+const MongoClient = require('mongodb').MongoClient;
+
+const uri = "mongodb+srv://user-13:m97zcguwhakwp4@cluster0.bzm1t.mongodb.net/Cluster0?retryWrites=true&w=majority";
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
+});
 const mongoose = require('mongoose');
 
 // Where does this come from?
@@ -19,3 +30,4 @@ const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB Connection Error'));
 
 module.exports = db;
+
